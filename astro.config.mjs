057bfee11dfdedcfc10fd993a1cc34d4d3b835f0
Rodrigo Santos <20969828+rodrigosantostech@users.noreply.github.com>
2026-04-15 14:41:43 -0300

    fix: update site URL and base path in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://rodrigosantostech.github.io",
-  base: "/tech"
+  site: "https://rodrigosantostech.github.io/rd-devlog/",
+  base: "/rd-devlog"
 });
